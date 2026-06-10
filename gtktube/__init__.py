e6ae8f417f1a8bcc9b4c44026d02b5c95ee43a1e
Derek Anderson <public@kered.org>
2026-06-10 12:24:49 -0700

    Use package metadata for version

--- gtktube/__init__.py
+++ gtktube/__init__.py
@@ -1,3 +1,8 @@
 __all__ = ["__version__"]
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("gtktube")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
